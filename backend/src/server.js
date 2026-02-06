6173408e29da6784462fc10d8037d23d6fb888dd
Sushant Kumar Singh <maidani486001@gmail.com>
2026-02-06 22:43:41 +0530

    fix CORS for vercel frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,24 +7,22 @@ import msgRoutes from "./routes/msg.route.js";
 dotenv.config();
 const app = express();
 
-app.use(express.json());
 app.use(cors({
   origin: [
-    "http://localhost:5173",                 // local frontend
-    "https://chatconnect-theta.vercel.app",  // production frontend
+    "http://localhost:5173",
+    "https://chatconnect-theta.vercel.app"
   ],
-  credentials: true,
+  credentials: true
 }));
 
-const port = process.env.PORT || 10000;
+app.use("/api/auth", authRoutes);
+app.use("/api/msg", msgRoutes);
 
 app.get("/", (req, res) => {
   res.send("ChatConnect backend is live 🚀");
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/msg", msgRoutes);
-
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`server is running at port: ${port}`);
 });
