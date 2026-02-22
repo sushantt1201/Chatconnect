219c1c62b7bb3889a73b97c30287768f39efc4e7
Sushant Kumar Singh <maidani486001@gmail.com>
2026-02-23 00:28:13 +0530

    frontend setup done

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+export const useAuthStore=create((set)=>({
+
+    authUser:{name:"SIJ",_id:123,age:25},
+    isLoggedIn:false,
+    isLoading:false,
+
+    login:()=>{
+        console.log("we just logged in:");
+        set({isLoggedIn:true,isLoading:true});
+        //sfn
+        //sfn
+        set({isLoggedIn:true,isLoading:true});
+    },
+}));
